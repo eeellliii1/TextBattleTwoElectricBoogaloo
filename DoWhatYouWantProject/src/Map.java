8723fbc42e1c1493de86b96730d13cb41a547c11
s-araoe <s-araoe@bsd405.org>
2017-06-06 10:58:29 -0700

    Attempted to make map and explorer method. We need to add a way to track and position the hero.

--- DoWhatYouWantProject/src/Map.java
+++ DoWhatYouWantProject/src/Map.java
@@ -1,5 +1,14 @@
 
-public class Map extends Tile{
-	private map = Tile[50][50];
-	
+public class Map {
+	private static Tile[][] map = new Tile[50][50];
+
+	public static void main(String[] args) {
+		for (int x = 0; x < 50; x++) {
+			for (int y = 0; y < 50; y++) {
+				map[x][y] = new Tile("Plains", true);
+			}
+			
+
+		}
+	}
 }
--- DoWhatYouWantProject/src/Tile.java
+++ DoWhatYouWantProject/src/Tile.java
@@ -1,4 +1,30 @@
 
-public class Tile{
-	
+public class Tile {
+	private String contents;
+	private boolean isPassable;
+	public Tile(String c, boolean p)
+	{
+		contents = c;
+		isPassable = p;
+	}
+	public String getContents() {
+		return contents;
+	}
+
+	public boolean checkPass() {
+		return isPassable;
+	}
+
+	public void setContents(String con) {
+		contents = con;
+	}
+
+	public void setPass(int b) {
+		// 1 is passable 0 is not
+		if (b == 1) {
+			isPassable = true;
+		} else {
+			isPassable = false;
+		}
+	}
 }
